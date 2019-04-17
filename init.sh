302bebfefd36a7e0178ac2feb79142342262efbe
Thibaut <thibaut.villeneuve@epsi.fr>
2019-04-17 15:29:09 +0200

    add init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 #/bin/bash
-
+alias -p mke="./minikube.exe"
 alias -p k="./kubectl.exe"
 k -n default exec -it $(k get pod -l io.kompose.service=db -o jsonpath='{.items[0].metadata.name}') -- mysql -u root -proot < i4_gestion/database/gestion_produits.sql
